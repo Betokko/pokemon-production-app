e9bfc14469fa0ec49a8d2efff16595c0139d2cc3
Artur Nikitin <branewor@gmail.com>
2023-05-01 22:05:45 +0400

    fix redux store

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -1,10 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit'
-import { StateSchema } from 'app/providers/StoreProvider/config/StateSchema'
-
-export function createReduxStore (initialState?: StateSchema) {
-    return configureStore({
-        reducer: {},
-        devTools: __IS_DEV__,
-        preloadedState: initialState
-    })
-}
+// import { configureStore } from '@reduxjs/toolkit'
+// import { StateSchema } from 'app/providers/StoreProvider/config/StateSchema'
+//
+// export function createReduxStore (initialState?: StateSchema) {
+//     return configureStore({
+//         reducer: {},
+//         devTools: __IS_DEV__,
+//         preloadedState: initialState
+//     })
+// }
