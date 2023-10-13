c0ceb3f0ae651aae5f26f160fd5a5101762909a2
Artur Nikitin <branewor@gmail.com>
2023-10-13 16:32:15 +0400

    hotfix: useInfiniteScroll

--- src/shared/lib/hooks/useInfiniteScroll.ts
+++ src/shared/lib/hooks/useInfiniteScroll.ts
@@ -10,9 +10,12 @@ export const useInfiniteScroll = ({ callback, wrapperRef, triggerRef }: IUseInfi
     const observer = useRef<IntersectionObserver | null>(null)
 
     useEffect(() => {
+        const wrapperElement = wrapperRef.current
+        const triggerElement = triggerRef.current
+
         if (callback) {
             const options = {
-                root: wrapperRef.current,
+                root: wrapperElement,
                 rootMargin: '10px 10px 10px 10px',
                 threshold: 1.0
             }
@@ -21,12 +24,12 @@ export const useInfiniteScroll = ({ callback, wrapperRef, triggerRef }: IUseInfi
                 if (entry.isIntersecting) callback()
             }, options)
 
-            observer.current.observe(triggerRef.current)
+            observer.current.observe(triggerElement)
         }
 
         return () => {
             // eslint-disable-next-line react-hooks/exhaustive-deps
-            if (observer.current) observer.current.unobserve(triggerRef.current)
+            if (observer.current && triggerElement) observer.current.unobserve(triggerElement)
         }
     }, [callback, triggerRef, wrapperRef])
 }
